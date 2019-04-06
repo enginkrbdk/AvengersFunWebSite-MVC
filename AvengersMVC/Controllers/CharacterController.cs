ea5befcbc121c5faf6d92c4ec547f27dce89d681
engkrbdk <e.k.1905@hotmail.com>
2019-04-06 18:56:43 +0300

    .

--- AvengersMVC/Controllers/CharacterController.cs
+++ AvengersMVC/Controllers/CharacterController.cs
@@ -13,5 +13,10 @@ namespace AvengersMVC.Controllers
         {
             return View();
         }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
     }
 }
